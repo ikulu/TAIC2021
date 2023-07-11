7f9cc78a0eb3c4f29ffe340e33aa5994f7c6b386
stanadim <stanjustine@gmail.com>
2023-07-11 20:28:24 +0300

    Fixing 1

--- resources/views/site/HeroSection.blade.php
+++ resources/views/site/HeroSection.blade.php
@@ -1,9 +1,9 @@
     <section id="hero">
     <div class="hero-container" data-aos="zoom-in" data-aos-delay="100">
-      <h1 class="hero-sub-title mb-1 mt-2 pb-0">{{$title}}</h1>
-        <p style="font-size:1.5rem;color:aquamarine">"{{$theme}}"</p>
+      <h1 class="mb-1 mt-2 pb-0">{{$title}}</h1>
+        <p style="font-size:1.5rem;color:rgb(47, 255, 186)">"{{$theme}}"</p>
       <p class="mb-1 pb-0">{{$location}}</p>
-      <p class="mb-1 pb-0" style=" color:rgb(95, 245, 95);font-size:2rem">
+      <p class="mb-1 pb-0" style=" color:rgb(95, 245, 95);">
         {{$presentedFromdate}}<sup style="color:white">th</sup> 
         <span style="color:white;">-</span>
          {{$presentedTodate}}<sup style="color:white">th</sup>
