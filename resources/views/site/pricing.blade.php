3340c7f77ef908e4fc504fe238f7f871b09df0b7
stanadim <stanjustine@gmail.com>
2023-07-17 19:05:41 +0300

    bg- pricing cards

--- resources/views/site/pricing.blade.php
+++ resources/views/site/pricing.blade.php
@@ -8,9 +8,10 @@
       <!-- <p>Nihil officia ut sint molestiae tenetur.</p> -->
     </div>
 
-    <div class="row contact-pricing contact-info">
+    <div class="row contact-info">
 
-      <div class="col-lg-6">
+      <div class="col-lg-6" 
+      style="background-color: #acdaed;margin:0px;border-radius: 1.2rem;text-align: center;">
         <div class="contact-address">
           <img src="{{asset('siteimg/icons/registered.png')}}" width="80px" height="80px" alt="Registered">
           <h3 style="padding:0px;margin:4px 0px;font-size:2.6rem;
@@ -19,7 +20,8 @@
           <address style="font-size: 1.4rem">Registered ICT Professional</address>
         </div>
       </div>
-      <div class="col-lg-6">
+      <div class="col-lg-6" 
+      style="background-color: #acdaed;margin:0px;border-radius: 1.2rem;text-align: center;">
         <div class="contact-address">
           <img src="{{asset('siteimg/icons/error.png')}}" width="80px" height="80px" alt="Non-Registered">
           <h3 style="padding:0px;margin:4px 0px;font-size:2.6rem;
