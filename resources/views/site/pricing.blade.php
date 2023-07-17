30d23dfea1d9a64dc5172ccc4cc4a8dd19e3ca8c
stanadim <stanjustine@gmail.com>
2023-07-17 19:01:13 +0300

    pricing styling

--- resources/views/site/pricing.blade.php
+++ resources/views/site/pricing.blade.php
@@ -8,19 +8,23 @@
       <!-- <p>Nihil officia ut sint molestiae tenetur.</p> -->
     </div>
 
-    <div class="row contact-pricing">
+    <div class="row contact-pricing contact-info">
 
       <div class="col-lg-6">
         <div class="contact-address">
           <img src="{{asset('siteimg/icons/registered.png')}}" width="80px" height="80px" alt="Registered">
-          <h3 >420,000 Tsh</h3>
+          <h3 style="padding:0px;margin:4px 0px;font-size:2.6rem;
+          border-radius: .8rem;color: #001466; font-weight: 500;">
+          420,000 Tsh</h3>
           <address style="font-size: 1.4rem">Registered ICT Professional</address>
         </div>
       </div>
       <div class="col-lg-6">
         <div class="contact-address">
           <img src="{{asset('siteimg/icons/error.png')}}" width="80px" height="80px" alt="Non-Registered">
-          <h3 >500,000 Tsh</h3>
+          <h3 style="padding:0px;margin:4px 0px;font-size:2.6rem;
+          border-radius: .8rem;color: #001466; font-weight: 500;">
+          500,000 Tsh</h3>
           <address style="font-size: 1.4rem">Non-Registered ICT Professional</address>
         </div>
       </div>
