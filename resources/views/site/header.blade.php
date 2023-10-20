e37c7c94c51f86da3733f561ab2c196dc51e3f2f
adim <stanjustine@gmail.com>
2023-10-20 08:21:44 +0300

    Photo gallery

--- resources/views/site/header.blade.php
+++ resources/views/site/header.blade.php
@@ -23,6 +23,19 @@
           <li><a class="nav-link " href="{{route('exhibiting')}}">Exhibitors</a></li>
           <li><a class="nav-link " href="{{route('hospitality')}}">Hospitality</a></li>
           <li><a class="nav-link " href="{{route('tours')}}">Tours</a></li>
+          <li class="dropdown"><a href="#"><span>Gallery</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li class="dropdown">
+                <a href="https://infocusstudiotz.client-gallery.com/gallery/women-and-youth-in-ict" target="_blank"><span>Women's in ICT</span></a>
+              </li>
+              <li class="dropdown">
+                <a href="https://infocusstudiotz.client-gallery.com/gallery/7th-taic-2023-official-opening" target="_blank"><span>Official Opening</span></a>
+              </li>
+              <li class="dropdown">
+                <a href="https://drive.google.com/drive/folders/16jloaMsqZcUfOs46zJyIG7VS1HCtfsKe?usp=share_link" target="_blank"><span>General</span></a>
+              </li>
+            </ul>
+          </li>
           {{-- <li><a class="nav-link scrollto" href="#speakers">Speakers</a></li> --}}
           {{-- <li><a class="nav-link scrollto" href="#schedule">Schedule</a></li> --}}
           {{-- <li><a class="nav-link scrollto" 
